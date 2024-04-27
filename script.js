2593f315936776970944febf3709e60b2e29d449
brandon-parkerson <166299546+brandon-parkerson@users.noreply.github.com>
2024-04-27 18:28:51 +0300

    Show sum on display.

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const equation = [];
-
+const display = document.querySelector("#display");
+const equals = document.querySelector(".equals");
 
 function add(num1, num2) {
-    return num1 + num2;
+    let number1 = parseInt(num1);
+    let number2 = parseInt(num2);
+    let sum = number1 + number2;
+    console.log(sum);
+    display.innerHTML = `${sum}`;
+    return sum;
 };
 
 function subtract(num1, num2) {
@@ -18,13 +23,23 @@ function divide(num1, num2) {
 };
 
 function moveToDisplay(button) {
-    let num = button.value;
-    document.getElementById("display").innerHTML += num;
-    equation.push(num);
+    let num = button.value
+    display.innerHTML += num;
+    //let content = display.textContent;
+    //console.log(content);
+    
+    //equals.addEventListener("click", operate(value))
+    //let splitValue = value.split(" ");
+    //operate(splitValue);
 };
 
-function operate(num1, num2, operator) {
 
+function operate(content) {
+    splitContent = content.split(" ");
+    if (splitContent.includes("+")) {
+        add(splitContent[0], splitContent[2]);
+    };
+    console.log(splitContent);
 }
 
 
