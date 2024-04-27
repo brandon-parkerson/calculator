6eaddef5061eff58e6e5d827ee8a811f12f84751
brandon-parkerson <166299546+brandon-parkerson@users.noreply.github.com>
2024-04-27 19:19:29 +0300

    Clear display when clear button is pressed. Catch divided by zero error.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const display = document.querySelector("#display");
 const equals = document.querySelector(".equals");
+const clear = document.getElementsByClassName("clear operator");
+
+
+
 
 function add(num1, num2) {
     let number1 = parseInt(num1);
@@ -29,8 +33,15 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     let number1 = parseInt(num1);
     let number2 = parseInt(num2);
-    let answer = number1 / number2;
-    display.innerHTML =`${answer.toFixed(5)}`;
+    if (number2 === 0) {
+        display.innerHTML = `You are an idiot lol`;
+        
+    }
+    else {
+        let answer = number1 / number2;
+        display.innerHTML =`${answer.toFixed(5)}`;
+    };
+    
 };
 
 function moveToDisplay(button) {
@@ -60,6 +71,11 @@ function operate(content) {
         multiply(splitContent[0], splitContent[2]);
     }
     console.log(splitContent);
+    
 }
+
+function clearDisplay() {
+    display.innerHTML = "";
+};
 
 
