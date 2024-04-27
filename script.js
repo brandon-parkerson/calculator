6d374e868eb1a014062eb86c51531af632a4bbd0
brandon-parkerson <166299546+brandon-parkerson@users.noreply.github.com>
2024-04-27 20:54:43 +0300

    Shadow styling.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function add(num1, num2) {
     let number1 = parseInt(num1);
     let number2 = parseInt(num2);
     let sum = number1 + number2;
-    console.log(sum);
+    
     display.innerHTML = `${sum}`;
 
 };
@@ -26,7 +26,7 @@ function multiply(num1, num2) {
     let number1 = parseInt(num1);
     let number2 = parseInt(num2);
     let product = number1 * number2;
-    console.log(product);
+    
     display.innerHTML = `${product}`;
 };
 
@@ -47,12 +47,7 @@ function divide(num1, num2) {
 function moveToDisplay(button) {
     let num = button.value
     display.innerHTML += num;
-    //let content = display.textContent;
-    //console.log(content);
     
-    //equals.addEventListener("click", operate(value))
-    //let splitValue = value.split(" ");
-    //operate(splitValue);
 };
 
 
@@ -70,7 +65,7 @@ function operate(content) {
     else if (splitContent.includes("X")) {
         multiply(splitContent[0], splitContent[2]);
     }
-    console.log(splitContent);
+    
     
 }
 
