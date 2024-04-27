e023681ed39139870cb84431f3ee7d9cb22bc6bb
brandon-parkerson <166299546+brandon-parkerson@users.noreply.github.com>
2024-04-27 18:49:44 +0300

    Add all calculation features.

--- script.js
+++ script.js
@@ -7,19 +7,30 @@ function add(num1, num2) {
     let sum = number1 + number2;
     console.log(sum);
     display.innerHTML = `${sum}`;
-    return sum;
+
 };
 
 function subtract(num1, num2) {
-    return num1 - num2;
-};
+    let number1 = parseInt(num1);
+    let number2 = parseInt(num2);
+    let difference = number1 - number2;
+    display.innerHTML = `${difference}`;
+}
+    
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    let number1 = parseInt(num1);
+    let number2 = parseInt(num2);
+    let product = number1 * number2;
+    console.log(product);
+    display.innerHTML = `${product}`;
 };
 
 function divide(num1, num2) {
-    return num1 / num2;
+    let number1 = parseInt(num1);
+    let number2 = parseInt(num2);
+    let answer = number1 / number2;
+    display.innerHTML =`${answer.toFixed(5)}`;
 };
 
 function moveToDisplay(button) {
@@ -38,7 +49,16 @@ function operate(content) {
     splitContent = content.split(" ");
     if (splitContent.includes("+")) {
         add(splitContent[0], splitContent[2]);
-    };
+    }
+    else if (splitContent.includes("-")) {
+        subtract(splitContent[0], splitContent[2]);
+    }
+    else if (splitContent.includes("/")) {
+        divide(splitContent[0], splitContent[2]);
+    }
+    else if (splitContent.includes("X")) {
+        multiply(splitContent[0], splitContent[2]);
+    }
     console.log(splitContent);
 }
 
